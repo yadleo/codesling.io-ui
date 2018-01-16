be8d0603c8f108ffff6f2d11b92cc2f8598713c0
Justin Kang <jmkang06@gmail.com>
2018-01-16 02:37:18 -0800

    [ops] - server to serve static files

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// import express from 'express';
+// import path from 'path';
+
+const express = require('express');
+const path = require('path');
+
+const server = express();
+const PORT = process.env.PORT || 3000
+
+server.use(express.static(path.join(__dirname, '../client/public')));
+
+server.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/public/index.html')));
+
+server.listen(PORT, () => console.log('serving static files on port ', PORT));
