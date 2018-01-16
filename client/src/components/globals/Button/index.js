77e33007b1476417f40faaf2e7495cbfec2ba7ba
Justin Kang <jmkang06@gmail.com>
2018-01-16 02:41:28 -0800

    [ops] - global components - button - loading - logo - forms - protected components

---
+++ client/src/components/globals/Button/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import Loading from '../Loading';
+
+import './Button.css';
+
+const Button = ({
+  loading,
+  text,
+  color,
+  error,
+  type,
+  className,
+  onClick,
+  id,
+  backgroundColor
+}) => {
+  if (loading) {
+    backgroundColor = 'gray';
+  }
+  if (error) {
+    backgroundColor = 'red';
+  }
+
+  return (
+    <div className={`button-container ${className ? className : ''}`}>
+      <button 
+        id={id}
+        className={`${color}-text palette-${backgroundColor} ${!text ? 'no-text' : ''}`}
+        onClick={onClick}
+        type={type}
+      >
+        {loading ? (
+          <Loading color="black" />
+        ) : (
+          error ? error : (
+            <span>
+              {text}
+            </span>
+          )
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ client/src/components/globals/Loading/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Loading.css';
+
+const Loading = ({ color }) => {
+  let cssStyles = {
+    backgroundColor: color === 'black' ? '#000' : '#fff',
+  };
+  return (
+    <div className="spinner">
+      <div style={cssStyles} className="double-bounce1"></div>
+      <div style={cssStyles} className="double-bounce2"></div>
+    </div>
+  );
+};
+
+export default Loading;
