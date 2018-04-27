e860b5882695405a6ef68c446eb887b1454ba616
Nikko Sanchez <neeks08@gmail.com>
2018-04-27 11:33:43 -0700

    History Page for user now complete just needs endpoint to display challenger name rather than challenger id

--- client/src/components/History/HistoryList.jsx
+++ client/src/components/History/HistoryList.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 export const HistoryList = ({ history }) => {
   let outcome;
   return (
-    <div>
-      {history.map(hist => {
-        outcome = hist.outcome === 0 ? 'Loss' : 'Win';
-        return (
-          <li>
-            <div>{outcome}</div>
-            <div>Opponent: {hist.receiver.rows[0].username}</div>
-            <div>Clout Earned: {hist.clout}</div>
-          </li>
-        )})}
-    </div>
+    history.map((hist, key) => {
+      outcome = hist.outcome === 0 ? 'Loss' : 'Win';
+      console.dir(hist)
+      return (
+        <tr key={key}>
+          <td align='center'>{hist.challenger_id}</td>
+          <td align='center'>{hist.clout}</td>
+          <td align='center'>{outcome}</td>
+        </tr>
+      )}
+    )
   )
 };
